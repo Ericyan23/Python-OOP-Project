2ebbe27cb8a725f25522fa8cbb0a8956d062b3a9
Sihan Yan <ericyan2112@gmail.com>
2024-01-14 14:55:42 -0700

    new project Tic-Tac-Toe

---
+++ Tic-Tac-Toe/main.py
@@ -0,0 +1,30 @@
+class Move:
+
+    def __init__(self, value):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    def is_valid(self):
+        if 1<= self._value <= 9:
+            return True
+        else:
+            return False
+
+    def find_row(self):
+        if self._value in (1,2,3):
+            return 0
+        elif self._value in (4,5,6):
+            return 1
+        else:
+            return 2
+
+    def find_colum(self):
+        if self._value in (1,4,7):
+            return 0
+        elif self._value in (2,5,8):
+            return 1
+        else:
+            return 2
