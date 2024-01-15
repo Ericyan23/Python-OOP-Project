90adb841e585c6fa673341db24f2f320de4719ed
Sihan Yan <ericyan2112@gmail.com>
2024-01-14 20:57:47 -0700

    Finish Board class

--- Tic-Tac-Toe/board.py
+++ Tic-Tac-Toe/board.py
@@ -21,6 +21,66 @@ class Board:
     def print_game_board_with_position(self):
         print("| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n| 7 | 8 | 9 |")
 
+    def submit_move(self, player, move):
+        row = move.get_row()
+        col = move.get_colum()
+        value = self.game_board[row][col]
+        if value == Board.empty_cell:
+            self.game_board[row][col] = player.marker
+        else:
+            print("This position is already taken. Please enter another one")
+
+    def check_game_over(self, player, last_move):
+        return (
+            (self.check_row(player, last_move))
+            or (self.check_col(player, last_move))
+            or (self.check_diagonal(player))
+            or (self.check_antidiagonal(player))
+        )
+
+    def check_row(self, player, last_move):
+        row = last_move.get_row()
+        return self.game_board[row].count(player.marker) == 3
+
+    def check_col(self, player, last_move):
+        col = last_move.get_colum()
+        marker_count = 0
+
+        for i in range(3):
+            if self.game_board[i][col] == player.marker:
+                marker_count += 1
+
+        return marker_count == 3
+
+    def check_diagonal(self, player):
+        marker_count = 0
+        for i in range(3):
+            if self.game_board[i][i] == player.marker:
+                marker_count += 1
+        return marker_count == 3
+
+    def check_antidiagonal(self, player):
+        marker_count = 0
+        for i in range(3):
+            if self.game_board[i][2 - i] == player.marker:
+                marker_count += 1
+        return marker_count == 3
+
+
+from move import Move
+from player import Player
+
+player = Player()
+move1 = Move(1)
+move2 = Move(4)
+move3 = Move(7)
+
 
 board = Board()
 board.print_game_board()
+board.submit_move(player, move1)
+board.submit_move(player, move2)
+board.submit_move(player, move3)
+board.print_game_board()
+
+print(board.check_game_over(player, move3))
--- Tic-Tac-Toe/move.py
+++ Tic-Tac-Toe/move.py
@@ -12,7 +12,7 @@ class Move:
         else:
             return False
 
-    def find_row(self):
+    def get_row(self):
         if self._value in (1, 2, 3):
             return 0
         elif self._value in (4, 5, 6):
@@ -20,7 +20,7 @@ class Move:
         else:
             return 2
 
-    def find_colum(self):
+    def get_colum(self):
         if self._value in (1, 4, 7):
             return 0
         elif self._value in (2, 5, 8):
